40a541a6a2a5087280f78a40603a5d2d0aec3533
DarkChild <caiopcmx@gmail.com>
2025-05-11 11:09:53 -0300

    Update projeto_2.py
    Tentei colocar um mini verificador, tente acrescentar no seu código

--- projeto_2.py
+++ projeto_2.py
@@ -1,13 +1,13 @@
-introdução = "COMPARADOR DE VALORES"
-print(introdução)
-valor1 = input("Insira o primeiro valor: ")
-valor2 = input("Insira o segundo valor: ")
+valor1 = int(input("Insira o primeiro valor (número inteiro): "))
+valor2 = int(input("Insira o segundo valor (número inteiro): "))
 
-if valor1 > valor2:
-    print(f"{valor1} é maior que {valor2}")
-elif valor1 < valor2:
-    print(f'{valor1} é menor que {valor2}')
-elif valor1 == valor2:
-    print("os dois valores são iguais")
-else:
-    print("Não aplicavel")
+    # Diferença entre os dois valores inseridos
+    if valor1 > valor2:
+        print(f"\n{valor1} é maior que {valor2}")
+    elif valor1 < valor2:
+        print(f"\n{valor1} é menor que {valor2}")
+    else:
+        print("\nOs dois valores são iguais.")
+except ValueError:
+    # Caso o usuário insira algo que não seja um número inteiro
+    print("\nErro: Por favor, insira apenas números inteiros válidos.")
